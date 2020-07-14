5447dc24c45e4abdd464e92f96f026edda7e85c4
Pavel Shlyak <shlyakpavel@reg.ru>
2020-07-14 17:43:07 +0400

    Proof of concept

---
+++ src/translations/organizer.ts
@@ -0,0 +1,485 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>DialogAppointment</name>
+    <message>
+        <location filename="../../build/src/ui_dialogappointment.h" line="213"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogappointment.h" line="214"/>
+        <source>Appointment Date:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogappointment.h" line="215"/>
+        <source>theDate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogappointment.h" line="216"/>
+        <source>Title:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogappointment.h" line="217"/>
+        <source>Location:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogappointment.h" line="218"/>
+        <source>Notes:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogappointment.h" line="219"/>
+        <source>All Day</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogappointment.h" line="220"/>
+        <source>Start Time:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogappointment.h" line="221"/>
+        <source>End Time: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogappointment.h" line="222"/>
+        <source>Reminder: </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DialogAppointmentUpdate</name>
+    <message>
+        <location filename="../../build/src/ui_dialogappointmentupdate.h" line="215"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogappointmentupdate.h" line="216"/>
+        <source>Appointment Date:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogappointmentupdate.h" line="217"/>
+        <source>theDate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogappointmentupdate.h" line="218"/>
+        <source>Title:         </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogappointmentupdate.h" line="219"/>
+        <source>Location: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogappointmentupdate.h" line="220"/>
+        <source>Notes: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogappointmentupdate.h" line="221"/>
+        <source>All Day</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogappointmentupdate.h" line="222"/>
+        <source>Start Time:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogappointmentupdate.h" line="223"/>
+        <source>End Time:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogappointmentupdate.h" line="224"/>
+        <source>Reminder: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogappointmentupdate.h" line="225"/>
+        <source>Delete Appointment</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DialogContact</name>
+    <message>
+        <location filename="../../build/src/ui_dialogcontact.h" line="274"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogcontact.h" line="275"/>
+        <source>First Name:     </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogcontact.h" line="276"/>
+        <source>Middle Names:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogcontact.h" line="277"/>
+        <source>Last Name:     </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogcontact.h" line="278"/>
+        <source>Email:             </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogcontact.h" line="279"/>
+        <source>Birthday Unknown </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogcontact.h" line="280"/>
+        <source>Born On:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogcontact.h" line="281"/>
+        <source>Add To Calendar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogcontact.h" line="282"/>
+        <source>Street:     </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogcontact.h" line="283"/>
+        <source>District:   </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogcontact.h" line="284"/>
+        <source>City:        </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogcontact.h" line="285"/>
+        <source>County:   </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogcontact.h" line="286"/>
+        <source>Postcode:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogcontact.h" line="287"/>
+        <source>Country:  </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogcontact.h" line="288"/>
+        <source>Phone:    </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogcontact.h" line="289"/>
+        <source>Delete Contact</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DialogPreferences</name>
+    <message>
+        <location filename="../../build/src/ui_dialogpreferences.h" line="122"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogpreferences.h" line="123"/>
+        <source>Font Size:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogpreferences.h" line="124"/>
+        <source>Locale:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogpreferences.h" line="125"/>
+        <source>Play Audio</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogpreferences.h" line="126"/>
+        <source>Dark Calendar </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogpreferences.h" line="127"/>
+        <source>Line Spacing</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DialogRepeatAppointment</name>
+    <message>
+        <location filename="../../build/src/ui_dialogrepeatappointment.h" line="241"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogrepeatappointment.h" line="242"/>
+        <source>Appointment Start Date:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogrepeatappointment.h" line="243"/>
+        <source>the Date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogrepeatappointment.h" line="244"/>
+        <source>Title:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogrepeatappointment.h" line="245"/>
+        <source>Location:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogrepeatappointment.h" line="246"/>
+        <source>Notes:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogrepeatappointment.h" line="247"/>
+        <source>All Day</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogrepeatappointment.h" line="248"/>
+        <source>Start Time:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogrepeatappointment.h" line="249"/>
+        <source>End Time: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogrepeatappointment.h" line="250"/>
+        <source>Repeat Every:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogrepeatappointment.h" line="251"/>
+        <source>Days</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogrepeatappointment.h" line="252"/>
+        <source>Occurrences</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DialogUpcomingBirthdays</name>
+    <message>
+        <location filename="../../build/src/ui_dialogupcomingbirthdays.h" line="57"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DialogUpcomingSchedule</name>
+    <message>
+        <location filename="../../build/src/ui_dialogupcomingschedule.h" line="94"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogupcomingschedule.h" line="95"/>
+        <source>Upcoming  Schedule For: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_dialogupcomingschedule.h" line="96"/>
+        <source>Date</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="326"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="327"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="328"/>
+        <source>Delete All Appointments</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="329"/>
+        <source>Delete All Contacts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="330"/>
+        <source>New Appointment (Double Click)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="331"/>
+        <source>Generate Repeat Appointments</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="332"/>
+        <source>Upcoming Schedule</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="333"/>
+        <source>New Contact</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="334"/>
+        <source>Check For Birthdays</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="335"/>
+        <source>Next Month</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="336"/>
+        <source>Previous Month</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="337"/>
+        <source>Today</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="338"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="339"/>
+        <source>Export Appointments</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="340"/>
+        <source>Import Appointments</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="341"/>
+        <source>Export Contacts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="342"/>
+        <source>Import Contacts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="343"/>
+        <source>Preferences</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="344"/>
+        <source>Increase Font</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="345"/>
+        <source>Decrease Font</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="346"/>
+        <source>Reset Font</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="347"/>
+        <source>Show Birthdays</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="348"/>
+        <source>Show Holidays</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="349"/>
+        <source>MonthYear</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="350"/>
+        <location filename="../../build/src/ui_mainwindow.h" line="359"/>
+        <source>Calendar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="351"/>
+        <source>Show Quick/Full View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="352"/>
+        <source>Mail To:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="353"/>
+        <location filename="../../build/src/ui_mainwindow.h" line="357"/>
+        <source>Contacts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="354"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="355"/>
+        <source>Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="356"/>
+        <source>Appointments</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="358"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
